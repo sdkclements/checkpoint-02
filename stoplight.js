750f0fd471641bbdfc0ada8f968c01b9e3050930
Samuel Clements <sdkclements@gmail.com>
2017-01-30 09:44:50 -0500

    timer commented out, sorta funky

--- stoplight.js
+++ stoplight.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
 
-            var light = $('.bulb');
+    var light = $('.bulb');
 
-            var handleBulbClick = function() {
-                $(this).addClass('active')
-                $(this).siblings().removeClass('active');
-            }
-            light.on('click', handleBulbClick)
-            
-            function yellowToRed(){
-            	if $('#yellow').hasClass('active')
+    var handleBulbClick = function() {
+        $(this).addClass('active')
+        $(this).siblings().removeClass('active');
+    }
+    light.on('click', handleBulbClick)
+    
 
+    // var yellow = $('#yellow');
 
-            }
-        	setInterval(sayHello, 2000)
-            
-        })
+    // var yellowToRed = function() {
+    //     if ( $('#yellow').hasClass('active') ) {
+    //         // $(this).delay(2000)
+    //         console.log('hi 1')
+    //         $(this).removeClass('active').delay(2000)
+             
+    //         $('#red').addClass('active');
+    //          console.log('hi 3')
+    //     } else {
+    //         console.log("not yello")
+
+    //     }
+
+    // }
+    // yellow.on('click', yellowToRed)
+
+})
